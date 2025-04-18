40c1f0472cf1a49e5d61dba3bfca18ab7ca42dfb
Alperen Can <alperencan2020@gmail.com>
2025-04-18 18:22:14 +0300

    Add project files.

---
+++ mvc_bitirme_projesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_bitirme_projesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
